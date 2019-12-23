817646b0e2b842b399479da9904d06bcd7c82567
Muhamed Akl <48137672+MuAkl@users.noreply.github.com>
2019-12-23 12:22:16 +0200

    Add files via upload

--- src/android/OTPAutofill.java
+++ src/android/OTPAutofill.java
@@ -1,6 +1,6 @@
 package com.muakl.cordova.plugin;
 
-//import android.widget.Toast;
+import android.widget.Toast;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.cordova.CallbackContext;
@@ -17,7 +17,7 @@ public boolean execute(String action,JSONArray args,
         // Verify that the user sent a 'detect' action
         if(!action.equals("detect")){
             callbackContext.error("\"" + action + "\" is not a recognized action.");
-            return false;
+            return false;//new PluginResult(PluginResult.Status.INVALID_ACTION);
         }
         String message;
         try{
@@ -25,7 +25,7 @@ public boolean execute(String action,JSONArray args,
             message = options.getString("message");
         }  catch(JSONException e){
             callbackContext.error("Error encountered: " + e.getMessage());
-            return false;
+            return false;//new PluginResult(PluginResult.Status.ERROR);
         }
             Pattern p = Pattern.compile("\\d{3,8}");
             Matcher m = p.matcher(message);
@@ -39,11 +39,11 @@ public boolean execute(String action,JSONArray args,
             }
             
             if(OTP !=""){
-                //Toast toast = Toast.makeText(cordova.getActivity(), OTP,Toast.LENGTH_LONG);
-               // toast.show();
+                Toast toast = Toast.makeText(cordova.getActivity(), OTP,Toast.LENGTH_LONG);
+                toast.show();
                 PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
                 callbackContext.sendPluginResult(pluginResult,OTP);
-                return true;
+                return true;//new PluginResult(PluginResult.Status.OK, OTP);
             }
             callbackContext.error("Couldn't detect otp");
             return false;
